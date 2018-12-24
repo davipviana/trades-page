ba4968e22df1f47f0250a1a01f17aed0dd3c0b8d
Davi Viana <davipviana@gmail.com>
2018-12-24 11:46:39 -0200

    Encapsulating Trade class

--- js/app/models/Trade.js
+++ js/app/models/Trade.js
@@ -1,11 +1,23 @@
 class Trade {
     constructor(date, amount, value) {
-        this.date = date;
-        this.amount = amount;
-        this.value = value;
+        this._date = date;
+        this._amount = amount;
+        this._value = value;
     }
 
     getVolume() {
-        return this.amount * this.value;
+        return this._amount * this._value;
+    }
+
+    getDate() {
+        return this._date;
+    }
+
+    getAmount() {
+        return this._amount;
+    }
+
+    getValue() {
+        return this._value;
     }
 }
