f6aabba7d7d2658a1c3a37033e8a80e3aae066b0
Davi Viana <davipviana@gmail.com>
2018-12-24 17:42:18 -0200

    Adjust regex date validation

--- js/app/helpers/DateHelper.js
+++ js/app/helpers/DateHelper.js
@@ -9,7 +9,7 @@ class DateHelper {
     }
 
     static textToDate(text) {
-        if(!/\d{4}-\d{2}-\d{2}/.test(text)) {
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(text)) {
             throw new Error('Input format invalid. Must be yyyy-mm-dd')
         }
         
