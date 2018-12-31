1bbe264488f7721761e11b9414ac669581a3040e
Davi Viana <davipviana@gmail.com>
2018-12-31 16:19:40 -0200

    Removing auto generated files

--- js/app/polyfill/es6.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-if (!Array.prototype.includes) {
-    console.log('Polyfill to Array.includes applied');
-    Array.prototype.includes = function (elem) {
-        return this.indexOf(elem) != -1;
-    };
-}
-//# sourceMappingURL=es6.js.map
